7d3cb9391b75f4ace6f4b4a64288534f447eb51c
yilinliu <yilin.liu@hotmail.com>
2024-03-27 13:54:50 -0400

    actitives

--- tampering/secure.js
+++ tampering/secure.js
@@ -1,37 +1,36 @@
-const express = require("express")
-const session = require("express-session")
+const express = require("express");
+const session = require("express-session");
 
-const app = express()
+const app = express();
 // Enter session secret key as an argument
 const secret = process.argv[2];
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: false }));
 
 app.use(
   session({
     secret: `${secret}`,
     cookie: {
-        httpOnly: true,
-        sameSite: true,
+      httpOnly: true,
+      sameSite: true,
     },
     resave: false,
-    saveUninitialized: false
+    saveUninitialized: false,
   })
-)
+);
 
 app.post("/sensitive", (req, res) => {
-  if (req.session.user === 'Admin') {
-    req.session.sensitive = 'supersecret';
-    res.send({message: 'Operation successful'});
+  if (req.session.user === "Admin") {
+    req.session.sensitive = "supersecret";
+    res.send({ message: "Operation successful" });
+  } else {
+    res.send({ message: "Unauthorized Access" });
   }
-  else {
-    res.send({message: 'Unauthorized Access'});
-  }
-})
+});
 
 app.get("/", (req, res) => {
-  let name = "Guest"
+  let name = "Guest";
 
-  if (req.session.user) name = req.session.user
+  if (req.session.user) name = req.session.user;
 
   res.send(`
   <h1>Welcome, ${name}</h1>
@@ -42,24 +41,29 @@ app.get("/", (req, res) => {
   <form action="/forget" method="POST">
     <button>Logout</button>
   </form>
-  `)
-})
+  `);
+});
 
 app.post("/register", (req, res) => {
   const sanitizedName = escapeHTML(req.body.name.trim());
   req.session.user = sanitizedName;
-  res.send(`<p>Thank you</p> <a href="/">Back home</a>`)
-})
+  res.send(`<p>Thank you</p> <a href="/">Back home</a>`);
+});
 
 app.post("/forget", (req, res) => {
-  req.session.destroy(err => {
-    res.redirect("/")
-  })
-})
+  req.session.destroy((err) => {
+    res.redirect("/");
+  });
+});
 
 app.listen(8000);
 
 // Function to HTML escape user input
 function escapeHTML(input) {
-  return input.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;").replace(/'/g, "&#39;");
+  return input
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#39;");
 }
